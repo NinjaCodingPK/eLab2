6ead0bfd8e086b672cb2b9d2ae06f3a45b4a9dc0
wookie <kononenkoa995@ukr.net>
2016-04-26 23:42:27 +0300

    First commit

---
+++ src/main/Main.java
@@ -0,0 +1,11 @@
+package Main;
+
+/**
+ * Created by wookie on 4/26/16.
+ */
+public class Main {
+    public static void main(String args[]) {
+
+
+    }
+}
---
+++ src/model/Model.java
@@ -0,0 +1,54 @@
+package model;
+
+/**
+ * Created by wookie on 4/26/16.
+ */
+public class Model {
+    private int value1, value2, value3, value4;
+    private double mean;
+    private int rounded;
+
+    public Model() {
+
+    }
+
+    public Model(int _value1, int _value2, int _value3, int _value4) {
+        value1 = _value1;
+        value2 = _value2;
+        value3 = _value3;
+        value4 = _value4;
+    }
+
+    public void calculateMean() {
+
+
+    }
+
+    public void round() {
+
+    }
+
+    public int getValue1() {
+        return value1;
+    }
+
+    public int getValue2() {
+        return value2;
+    }
+
+    public int getValue3() {
+        return value3;
+    }
+
+    public int getValue4() {
+        return value4;
+    }
+
+    public double getMean() {
+        return mean;
+    }
+
+    public int getRounded() {
+        return rounded;
+    }
+}
